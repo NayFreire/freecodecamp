c7e2c8c82ad57733a9d119cd18995ef3f64425b9
NayhFreire <nayfreireof@gmail.com>
2024-03-08 18:13:41 -0300

    Fixing list of even and odd numbers

--- Scientific Computing with Python/lists/lists_1.py
+++ Scientific Computing with Python/lists/lists_1.py
@@ -1,9 +1,9 @@
-numbers = [x for x in range(0, 100)]
+numbers = [x for x in range(0, 50)]
 
 evens = lambda even: even % 2 == 0
-even_numbers = sorted(numbers, key=evens)
+even_numbers = list(filter(evens, numbers))
 
 odds = lambda odd: odd % 2 != 0
-odd_numbers = sorted(numbers, key=odds)
+odd_numbers = list(filter(odds, numbers))
 
 print(even_numbers, '\n', odd_numbers, 2 % 2 == 0)
