3862b833c75bb6ffeaa7c2a51e6325799890cba7
NayhFreire <nayfreireof@gmail.com>
2024-03-11 17:56:02 -0300

    The use os dictionaries

---
+++ Scientific Computing with Python/algorithm_design/dictionaries.py
@@ -0,0 +1,11 @@
+copper = {
+    'species': 'guinea pig', 
+    'age': 2
+}# dictionaries are mutable variables that store data with a key : value 
+
+copper['food'] = 'hay'
+copper['species'] = 'Cavia porcellus'
+
+del copper['age'] #deleting a key
+
+print(copper)
