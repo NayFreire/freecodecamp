dea227605569b0323589870c88b6cfa64edf5f76
NayhFreire <nayfreireof@gmail.com>
2024-03-11 17:57:42 -0300

    The use os dictionaries with graphs

---
+++ Scientific Computing with Python/algorithm_design/shortest_path.py
@@ -0,0 +1,8 @@
+my_graph = {
+    'A': ['B', 'D'], #A is linked with B and D
+    'B': ['A', 'C'], #B is linked with A and C
+    'C': ['B', 'D'], #C is linked with B and D
+    'D': ['A', 'C']  #D is linked with A and C
+}
+
+print(my_graph)
