f3772fb57cca650503b4fdabd960ac99b16a553f
NayhFreire <nayfreireof@gmail.com>
2024-03-11 18:19:46 -0300

    Using for loop to append items to a list

--- Scientific Computing with Python/algorithm_design/shortest_path.py
+++ Scientific Computing with Python/algorithm_design/shortest_path.py
@@ -1,8 +1,21 @@
 my_graph = {
-    'A': ['B', 'D'], #A is linked with B and D
-    'B': ['A', 'C'], #B is linked with A and C
-    'C': ['B', 'D'], #C is linked with B and D
-    'D': ['A', 'C']  #D is linked with A and C
+    'A': [('B', 3), ('D', 1)], #A is linked with B by 3 units of mesure and D by 1 unit of mesure
+    'B': [('A', 3), ('C', 4)], 
+    'C': [('B', 4), ('D', 7)], 
+    'D': [('A', 1), ('C', 7)]  
 }
 
 print(my_graph)
+
+def shortest_path(graph, start):
+    unvisited = [] #To visit all the nodes, we nee to know which ones have been visited...
+    distances = {}
+    for node in graph:
+        unvisited.append(node) #...that's why we add then all to the unvisited list
+        if node == start:
+            distances[node] = 0 #the starting node has no distance, cause it's the start
+        else:
+            distances[node] = float('inf') #in the beginning, every node is infinitly away from the start node*/
+    print(f'Unvisited: {unvisited}\nDistances: {distances}')
+
+shortest_path(my_graph, 'A')
