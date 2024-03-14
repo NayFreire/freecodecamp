e8bb5b203c167323987722931d2dde3fce537a70
NayhFreire <nayfreireof@gmail.com>
2024-03-14 19:53:30 -0300

    Verifying if the left or the right side are smaller, so the number can be correctly assigned

--- Scientific Computing with Python/data_structure/merge_sort.py
+++ Scientific Computing with Python/data_structure/merge_sort.py
@@ -12,6 +12,13 @@ def merge_sort(array):
     sorted_index = 0
 
     while left_array_index < len(left_part) and right_array_index < len(right_part):
-        if left_part[left_array_index] < right_part[right_array_index]:
-            pass
+        if left_part[left_array_index] < right_part[right_array_index]: #If the number on the left is smaller than the right... 
+            array[sorted_index] = left_part[left_array_index] #...the number on the left goes to its correct position...
+            left_array_index += 1 #... and the left_array_index is incremented by 1, so it can go to the next position
+
+        else: #If the number on the left is not smaller than the right... 
+            array[sorted_index] = right_part[right_array_index] #...the number on the tight goes to its correct position...
+            right_array_index += 1 #... and the right_array_index is incremented by 1, so it can go to the next position
+            
+        sorted_index += 1 # And after all, increment the sorted_index, so the next position in the array can be correctly assigned    
 merge_sort([3, 4, 2, 7, 10, 5, 1])
