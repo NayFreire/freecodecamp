ee03503041b5f9a6a5a3869808efc46f1ba8bf5d
NayhFreire <nayfreireof@gmail.com>
2024-03-14 18:53:09 -0300

    First division of the array

--- Scientific Computing with Python/data_structure/merge_sort.py
+++ Scientific Computing with Python/data_structure/merge_sort.py
@@ -1,5 +1,8 @@
 def merge_sort(array):
     middle_point = len(array) // 2 #get the middle point of the array so it can be divided by 2
-    print(array, middle_point, array[middle_point])
+    left_part = array[0: middle_point]
+    right_part = array[middle_point:len(array)] #also works with array[middle_point:]
+    
+    print(array, middle_point, array[middle_point], left_part, right_part)
 
 merge_sort([3, 4, 2, 7, 10, 5, 1])
