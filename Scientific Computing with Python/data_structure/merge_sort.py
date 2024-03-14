7adf3cd0525d2323afdab33fcba9199fd069478f
NayhFreire <nayfreireof@gmail.com>
2024-03-14 20:09:27 -0300

    Assinging remaining elements from both left and right

--- Scientific Computing with Python/data_structure/merge_sort.py
+++ Scientific Computing with Python/data_structure/merge_sort.py
@@ -21,4 +21,12 @@ def merge_sort(array):
             right_array_index += 1 #... and the right_array_index is incremented by 1, so it can go to the next position
             
         sorted_index += 1 # And after all, increment the sorted_index, so the next position in the array can be correctly assigned    
+    
+    while left_array_index < len(left_part): # while the left index hasn't got to the last element of the left_part...
+        array[sorted_index] = left_part[left_array_index] #... the sorted array will take the remaining elements of the left_part
+        sorted_index += 1 # And the sorted_index will be incremented by 1 so the elements can be assigned to it's correct places
+    
+    while right_array_index < len(right_part): # while the right index hasn't got to the last element of the right_part...
+        array[sorted_index] = right_part[right_array_index] #... the sorted array will take the remaining elements of the right_part
+        sorted_index += 1 # And the sorted_index will be incremented by 1 so the elements can be assigned to it's correct places
 merge_sort([3, 4, 2, 7, 10, 5, 1])
