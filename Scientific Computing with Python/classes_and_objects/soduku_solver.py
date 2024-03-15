4ec064b0761295a1ac5bb1d32dea0fa7db104438
NayhFreire <nayfreireof@gmail.com>
2024-03-15 17:52:05 -0300

    The use of __init__

---
+++ Scientific Computing with Python/classes_and_objects/soduku_solver.py
@@ -0,0 +1,4 @@
+class Board:
+    def __init__(self, board):
+        self.board = board # refers to the board attribute of the instance of the class. It's a variable that belongs to the object created from the Board class.
+        
