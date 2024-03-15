e87a06f41a839c79bc91bbd5e6303bffc971b011
NayhFreire <nayfreireof@gmail.com>
2024-03-15 17:59:24 -0300

    Setting the lines of the board

--- Scientific Computing with Python/classes_and_objects/soduku_solver.py
+++ Scientific Computing with Python/classes_and_objects/soduku_solver.py
@@ -1,4 +1,8 @@
 class Board:
     def __init__(self, board):
         self.board = board # refers to the board attribute of the instance of the class. It's a variable that belongs to the object created from the Board class.
-        
+    
+    def __str__(self): # This method is automatically called when you use the str() function on an instance of the class or when you use print() with the object.
+        upper_lines = f'\n╔═══{"╤═══"*2}{"╦═══"}{"╤═══"*2}{"╦═══"}{"╤═══"*2}╗\n'
+        middle_lines = f'╟───{"┼───"*2}{"╫───"}{"┼───"*2}{"╫───"}{"┼───"*2}╢\n'
+        lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
