4dacdb4eff014a850752b5860da8fb296c1f4e21
NayhFreire <nayfreireof@gmail.com>
2024-03-16 17:38:14 -0300

    "Operator must be '+' or '-'" error

--- Scientific Computing with Python/projects/arithmetic_formatter.py
+++ Scientific Computing with Python/projects/arithmetic_formatter.py
@@ -4,10 +4,13 @@ def arithmetic_arranger(problems, show_answers=False):
     try:
         if len(problems) > 5:
             raise ValueError("Too many problems.")
-        
+        for problem in problems:
+            print(problem)
+            if '*' in problem or '/' in problem:
+                raise ValueError("Operator must be '+' or '-'.")
     except ValueError as e:
         print("Error: ", e)
 
     return problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 * 43", "123 + 49", "45 * 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 * 43", "123 + 49"])}')
