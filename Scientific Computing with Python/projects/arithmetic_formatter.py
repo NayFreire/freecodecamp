0ec8c1d1bca6b33b13f175c2ce0260f381d80e6d
NayhFreire <nayfreireof@gmail.com>
2024-03-16 18:27:22 -0300

    "Numbers must only contain digits." error

--- Scientific Computing with Python/projects/arithmetic_formatter.py
+++ Scientific Computing with Python/projects/arithmetic_formatter.py
@@ -1,4 +1,13 @@
+import re
 import string
+
+digits = string.digits
+
+def is_it_just_numbers(number):
+    for n in number:
+        if n not in digits:
+            return False
+    return True
 
 def arithmetic_arranger(problems, show_answers=False):
     try:
@@ -8,9 +17,18 @@ def arithmetic_arranger(problems, show_answers=False):
             print(problem)
             if '*' in problem or '/' in problem:
                 raise ValueError("Operator must be '+' or '-'.")
+            
+            just_digits_1 = is_it_just_numbers(problem.split()[0])
+            just_digits_2 = is_it_just_numbers(problem.split()[2])
+
+            if not just_digits_1 or not just_digits_2:
+                raise ValueError("Numbers must only contain digits.")
+            
+            
     except ValueError as e:
         print("Error: ", e)
 
     return problems
 
-print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 * 43", "123 + 49"])}')
+print(f'\n{arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])}')
+# print(is_it_just_numbers('12.9'))
