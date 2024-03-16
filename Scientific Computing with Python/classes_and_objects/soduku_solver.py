78640d8b04465edc0a040aa912433e14754d121b
NayhFreire <nayfreireof@gmail.com>
2024-03-15 21:51:55 -0300

    Fix: Identation and vars in for + method to solve the sudoku

--- Scientific Computing with Python/classes_and_objects/soduku_solver.py
+++ Scientific Computing with Python/classes_and_objects/soduku_solver.py
@@ -8,7 +8,7 @@ class Board:
         lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
         board_string = upper_lines
 
-        for line, index in enumerate(self.board):
+        for index, line in enumerate(self.board):
             row_list = []
             for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start=1): #If the first line of sudoku is:
             # 1 | X | 5 | 3 | X | 4 | 8 | X | X 
@@ -18,17 +18,17 @@ class Board:
 
                 if square_no != 3: #if the square in the board isn't the last one...
                     row_list.append('║') #... put this character
-        row = f'║ {" ".join(row_list)} ║\n'
-        row_empty = row.replace('0', ' ') #replacing the 0 used for empty sparec with ' '.
+            row = f'║ {" ".join(row_list)} ║\n'
+            row_empty = row.replace('0', ' ') #replacing the 0 used for empty sparec with ' '.
 
-        board_string += row_empty
+            board_string += row_empty
 
-        if index < 8: #Verifying if we are at the last row of the board
-            if index % 3 == 2: # if we are at the last row of the 3x3 squares...
-                board_string += f'╠═══{"╪═══"*2}{"╬═══"}{"╪═══"*2}{"╬═══"}{"╪═══"*2}╣\n' # ... use this line for aesthetic purposes
-            else:
-                board_string += middle_lines
-        else: # If this is the last line oin the board...
+            if index < 8: #Verifying if we are at the last row of the board
+                if index % 3 == 2: # if we are at the last row of the 3x3 squares...
+                    board_string += f'╠═══{"╪═══"*2}{"╬═══"}{"╪═══"*2}{"╬═══"}{"╪═══"*2}╣\n' # ... use this line for aesthetic purposes
+                else:
+                    board_string += middle_lines
+            else: # If this is the last line oin the board...
                 board_string += lower_lines #... use the lower_lines
         return board_string
     
@@ -82,3 +82,30 @@ class Board:
                         return True #... the soduko is solved
                     self.board[row][col] = 0 # #If the recursive call returns false, the guess made the sudoku unsolvable, so the cell needs to go back to 0
             return False #Means that none of the guesses lead to a solution
+        
+def solve_sudoku(board):
+    gameboard = Board(board)
+    print(gameboard)
+    print(f'\nPuzzle to solve:\n{gameboard}')
+
+    if gameboard.solver():
+        print('\nSolved puzzle:')
+        print(gameboard)
+    else:
+        print('\nThe provided puzzle is unsolvable.')
+
+    return gameboard
+
+puzzle = [
+    [0, 0, 2, 0, 0, 8, 0, 0, 0],
+    [0, 0, 0, 0, 0, 3, 7, 6, 2],
+    [4, 3, 0, 0, 0, 0, 8, 0, 0],
+    [0, 5, 0, 0, 3, 0, 0, 9, 0],
+    [0, 4, 0, 0, 0, 0, 0, 2, 6],
+    [0, 0, 0, 4, 6, 7, 0, 0, 0],
+    [0, 8, 6, 7, 0, 4, 0, 0, 0],
+    [0, 0, 0, 5, 1, 9, 0, 0, 8],
+    [1, 7, 0, 0, 0, 6, 0, 0, 5]
+]
+
+solve_sudoku(puzzle)
