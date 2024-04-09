8a6b08f0fee9d4a1d47dcec216f85f9375bb3ed4
NayFreire <nayfreireof@gmail.com>
2024-04-09 20:19:25 -0300

    Comments added

--- Scientific Computing with Python/projects/time_calculator.py
+++ Scientific Computing with Python/projects/time_calculator.py
@@ -12,16 +12,16 @@ If the function is given the optional starting day of the week parameter, then t
 
 Do not import any Python libraries. Assume that the start times are valid times. The minutes in the duration time will be a whole number less than 60, but the hour can be any whole number.
 """
-def round_up(num, duration_min, start_min):
-    integer = int(num)
+def round_up(num, duration_min, start_min):    
+    if duration_min + start_min > 60: # In case this sum results in another hour to be added...
+        return True #... round it up
     
-    if duration_min + start_min > 60:
-        return True
+    integer = int(num) 
     
-    if num > integer:
-        return True
+    if num > integer: # In case the division between the duration hour and 24 isn't and integer...
+        return True #... round it up
     else:
-        return False
+        return False #... don't round it up
 
 def get_individual_numbers(start, duration):
     start_time, start_period = start.split()
@@ -31,7 +31,6 @@ def get_individual_numbers(start, duration):
     return start_period, start_hour, start_minute, duration_hour, duration_minute
 
 def add_time(start, duration, day=None):
-    next_day = False
     added_days = 0
     days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
@@ -42,21 +41,24 @@ def add_time(start, duration, day=None):
     new_hour = int(start_hour) + int(duration_hour)
     new_minute = int(start_minute) + int(duration_minute)
     
+    # Verifying if the minute is bigger than 59, so it can be corrected
     if new_minute > 59:
         while new_minute > 59:
             new_minute = new_minute - 60
-            new_hour = new_hour + 1
+            new_hour = new_hour + 1 # For every 60 min taken from the new_minute, 1 hour is added to the new_hour
     
+    # Verifying if the hour is bigger than 24, so it can be corrected
     if new_hour > 24:
-        need_to_round_up = round_up(int(duration_hour)/24, int(duration_minute), int(start_minute))
+        need_to_round_up = round_up(int(duration_hour)/24, int(duration_minute), int(start_minute)) # Function used to verify if there's need to round up and add another day
         
         while new_hour > 24:
             new_hour = new_hour - 24
-            added_days += 1
+            added_days += 1 # For every 24h taken from the new_hour, 1 day is added to the added_days
             
         if need_to_round_up:
             added_days += 1
     
+    # Verification for the 12 hour clock format
     if new_hour > 12 and start_period == 'PM':
         new_hour = new_hour - 12
         start_period = 'AM'
@@ -68,17 +70,21 @@ def add_time(start, duration, day=None):
     elif new_hour == 12 and start_period == 'PM':
         start_period = 'AM'
         
+    # Verification to add or not a 0 before the minute
     if new_minute < 10:
         new_minute = f"0{new_minute}"
 
     string_return = f"{new_hour}:{new_minute}{' ' + start_period}"
-
+    
+    # In case it has been one day
     if added_days == 1:
         string_return += ' (next day)'
     
+    # In case it has been more than one day
     if added_days > 1:
         string_return += f" ({added_days} days later)"
 
+    # If there's a day of the week
     if day:
         return f"{string_return}, {day}"
     else:
