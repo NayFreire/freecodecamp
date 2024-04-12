51e8e56385a38a2802949e1af0d98c6b2665bd29
NayFreire <nayfreireof@gmail.com>
2024-04-12 11:46:08 -0300

    Creating the init method

--- Scientific Computing with Python/projects/probability_calculator.py
+++ Scientific Computing with Python/projects/probability_calculator.py
@@ -1,13 +1,13 @@
 """
-Suppose there is a hat containing 5 blue balls, 4 red balls, and 2 green balls. What is the probability that a random draw of 4 balls will contain at least 1 red ball and 2 green balls? While it would be possible to calculate the probability using advanced mathematics, an easier way is to write a program to perform a large number of experiments to estimate an approximate probability.
+>Suppose there is a hat containing 5 blue balls, 4 red balls, and 2 green balls. What is the probability that a random draw of 4 balls will contain at least 1 red ball and 2 green balls? While it would be possible to calculate the probability using advanced mathematics, an easier way is to write a program to perform a large number of experiments to estimate an approximate probability.
 
-For this project, you will write a program to determine the approximate probability of drawing certain balls randomly from a hat.
+>For this project, you will write a program to determine the approximate probability of drawing certain balls randomly from a hat.
 
-First, create a Hat class in main.py. The class should take a variable number of arguments that specify the number of balls of each color that are in the hat.
+>First, create a Hat class in main.py. The class should take a variable number of arguments that specify the number of balls of each color that are in the hat.
 
-A hat will always be created with at least one ball. The arguments passed into the hat object upon creation should be converted to a contents instance variable. contents should be a list of strings containing one item for each ball in the hat. Each item in the list should be a color name representing a single ball of that color. For example, if your hat is {"red": 2, "blue": 1}, contents should be ["red", "red", "blue"].
+>A hat will always be created with at least one ball. The arguments passed into the hat object upon creation should be converted to a contents instance variable. contents should be a list of strings containing one item for each ball in the hat. Each item in the list should be a color name representing a single ball of that color. For example, if your hat is {"red": 2, "blue": 1}, contents should be ["red", "red", "blue"].
 
-The Hat class should have a draw method that accepts an argument indicating the number of balls to draw from the hat. This method should remove balls at random from contents and return those balls as a list of strings. The balls should not go back into the hat during the draw, similar to an urn experiment without replacement. If the number of balls to draw exceeds the available quantity, return all the balls.
+>The Hat class should have a draw method that accepts an argument indicating the number of balls to draw from the hat. This method should remove balls at random from contents and return those balls as a list of strings. The balls should not go back into the hat during the draw, similar to an urn experiment without replacement. If the number of balls to draw exceeds the available quantity, return all the balls.
 
 Next, create an experiment function in main.py (not inside the Hat class). This function should accept the following arguments:
 
@@ -30,7 +30,16 @@ import copy
 import random
 
 class Hat:
-    pass
+    def __init__(self, red=0, blue=0, green=0):
+        contents = []
+        for _ in range(red):
+            contents.append('red')
+        for _ in range(blue):
+            contents.append('blue')
+        for _ in range(green):
+            contents.append('green')
+
+        self.contents = contents
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
